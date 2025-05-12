fa3aeb668cc85179ec12c99734052227e70e1048
matthew-martin1184 <mattmartin1184@gmail.com>
2025-05-12 10:05:35 -0500

    Update dashboard.py

--- streamlit_dashboard/dashboard.py
+++ streamlit_dashboard/dashboard.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import matplotlib as plt
 
+file_path = "data/dataframes.pkl"
+with open(file_path, "rb") as f:
+    data = pickle.load(f)
+
+query_df = data['query_df']
+doctor_result_dict = data['doctor_result_dict']
+patient_result_dict = data['patient_result_dict']
+procedure_result_dict = data['procedure_result_dict']
+billing_result_dict = data['billing_result_dict']
+
+st.title("Healthcare Analytics Dashboard")
